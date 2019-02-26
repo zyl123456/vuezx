7d4abb40eaf9de1243665e2f46386aa0779a666f
zyl123456 <824353649@qq.com>
2019-02-26 10:14:32 +0800

    专项http

---
+++ index.js
@@ -0,0 +1,26 @@
+import './lib/common/common'
+
+import AuthEncrypt from './lib/auth/auth-encrypt'
+import TokenManager from './lib/auth/token-manager'
+
+import Http from './lib/http/http'
+
+import AjaxCache from './lib/cache/cache'
+import Storage from './lib/cache/storage'
+import TokenStorage from './lib/cache/token-storage'
+import Session from './lib/cache/session'
+
+import DateUtil from './lib/utils/date-util'
+import Map from './lib/utils/map'
+
+export {
+  AuthEncrypt,
+  TokenManager,
+  TokenStorage,
+  Http,
+  AjaxCache,
+  Storage,
+  Session,
+  DateUtil,
+  Map
+}
